33498c2357eca5b34c5624c3356676c79cc3c03d
^ ^AK <91782391+AK1000-discord@users.noreply.github.com>
2022-04-09 19:50:21 +0900

    Update day.py

--- day.py
+++ day.py
@@ -1,45 +1,48 @@
 #import
-import asyncio, discord, datetime, random
+import asyncio, discord, datetime, random, time
 from json import load
 import logging
 import re
+import sys
 from discord.ext import commands, tasks
 from discord.utils import get
 from discord import Member
 import calendar
 from colorama import Fore, Back, Style
 
+
+
+
 #onn
 with open('config.json') as f:
     d = load(f)
     token = d["token"]
     prefix = d["prefix"]
-    server = d["server"]
-calendar_month = calendar.month(2022, 3)
-bot = commands.Bot(command_prefix=prefix, self_bot=True, fetch_offline_members=False)
+calendar_month = calendar.month(2022, 4)
+AK = commands.Bot(command_prefix=prefix, self_bot=True, fetch_offline_members=False)
 
-
-   
 #timecommand
-@bot.command(pass_context=True)
+@AK.command(pass_context=True)
 async def day(ctx):
     await ctx.message.delete()
     dt = datetime.datetime.today() 
+    weekday = datetime.date.today().weekday()
+    weekdayname = calendar.day_name[weekday]
     await ctx.send(f'`{dt.year}年 {dt.month}月 {dt.day}日 {dt.hour}時 {dt.minute}分 {dt.second}秒`')
 
-@bot.command()
+@AK.command()
 async def time(ctx):
     await ctx.message.delete()
     dt = datetime.datetime.today()
     await ctx.send(f'`現在時刻\n{dt.hour}時 {dt.minute}分 {dt.second}秒`')
 
-@bot.command()
+@AK.command()
 async def cl(ctx):
     await ctx.message.delete()
     await ctx.send(f"`{calendar_month}`")
     
 #infocommand desuuuuuuuuuuuuuuuuuuuuuuuuuuuuuu
-@bot.command()
+@AK.command()
 async def sinfo(ctx):
     date_format = "%Y/%m/%d %I:%M.%S %p"
     name = str(ctx.guild.name)
@@ -54,15 +57,15 @@ async def sinfo(ctx):
     await ctx.message.delete()
     await ctx.send(f'`guildname:{name}\n owner:{owner}\n description:{description}\n guildid:{id}\n guildmember:{memberCount}\nguildcreated:{guildc.created_at.strftime(date_format)}`\n{sbanner}\n{sicon}')
     
-@bot.command()
+@AK.command()
 async def minfo(ctx, *, member: discord.Member = None):
     if member is None:
         member = ctx.author
     date_format = "%Y/%m/%d %I:%M.%S %p" 
-    req = await bot.http.request(discord.http.Route("GET", "/users/{uid}", uid=member.id))
+    req = await AK.http.request(discord.http.Route("GET", "/users/{uid}", uid=member.id))
     banner_id = req["banner"]
     banner_url = f"https://cdn.discordapp.com/banners/{member.id}/{banner_id}.png?size=1024"
-    req = await bot.http.request(discord.http.Route("GET", "/users/{uid}", uid=member.id))
+    req = await AK.http.request(discord.http.Route("GET", "/users/{uid}", uid=member.id))
     avatar_id = req["avatar"]
     guild = ctx.guild
     userguildavatar = f"https://cdn.discordapp.com/guilds/{guild.id}/users/{member.id}/avatars/{avatar_id}.png"
@@ -70,61 +73,66 @@ async def minfo(ctx, *, member: discord.Member = None):
     await ctx.message.delete()
     await ctx.send(f'`id:{member.id}\nname:{member}\nguildname:{member.display_name}\ntag:{member.discriminator}\nbot:{member.bot}\ncreateacctime:{member.created_at.strftime(date_format)}\njoinservertime:{member.joined_at.strftime(date_format)}`\n{banner_url}\n{avatar_url}')
 
-@bot.command(pass_context=True, name='status')
-async def status(ctx, member: Member):
-    await ctx.send(str(member.status))
+#async def dm():
+#    user = AK.get_user("")
+#    await user.send("H")
+
+@AK.command()
+async def ui(ctx, *, member: discord.Member = None):
+    if member is None:
+        member = ctx.author
+    req = await AK.http.request(discord.http.Route("GET", "/users/{uid}", uid=member.id))
+    avatar_id = req["avatar"]
+    url = f"https://cdn.discordapp.com/avatars/{member.id}/{avatar_id}.png?size=1024"
+    await ctx.message.delete()
+    await ctx.send(f"{url}\n{member}")
 
 
-@bot.command()
+@AK.command()
 async def gb(ctx):
     await ctx.message.delete()
     sbanner = ctx.guild.banner_url
     await ctx.send(f"{sbanner}")
 
-@bot.command()
-async def gi(ctx, member:discord.Member):
-    await ctx.send(member.avatar_url)
-
-@bot.command()
+@AK.command()
 async def ub(ctx, user:discord.Member):
     await ctx.message.delete()
     if user == None:
         user = ctx.author
-    req = await bot.http.request(discord.http.Route("GET", "/users/{uid}", uid=user.id))
+    req = await AK.http.request(discord.http.Route("GET", "/users/{uid}", uid=user.id))
     banner_id = req["banner"]
     # If statement because the user may not have a banner
     if banner_id:
         banner_url = f"https://cdn.discordapp.com/banners/{user.id}/{banner_id}?size=1024"
-    await ctx.send(f"||{banner_url}||")
-    await ctx.send(f"{user}")
+    await ctx.send(f"{banner_url}\n{user}")
 
-@bot.command()
+@AK.command()
 async def eu(ctx, emoji: discord.Emoji):
     await ctx.message.delete()
     await ctx.send(emoji.url)
 
 #imihucommand
-@bot.command()
+@AK.command()
 async def kami(ctx):
     await ctx.message.delete()
     await ctx.send(f"`|/    /_\    |\    /|   |\n|\   /    \  |  \ /  |  |`")
 #`|/    /_\    |\    /|   |
 #|\   /    \  |  \ /  |  |`
 
-@bot.command()
+@AK.command()
 async def bk(ctx, *, member: discord.Member = None):
     if member is None:
         member = ctx.author
     await ctx.message.delete()
     await ctx.send(f'<@{member.id}>はバカwwwwww')
 
-@bot.command()
+@AK.command()
 async def nani(ctx):
     await ctx.message.delete()
     await ctx.send(f"`/\/ /-\ /\/ |`")
 
 #system(?)
-@bot.command()
+@AK.command()
 @commands.has_permissions(ban_members = True)
 async def ban(ctx, member: discord.Member, *, reason = None):
     await ctx.message.delete()
@@ -132,15 +140,15 @@ async def ban(ctx, member: discord.Member, *, reason = None):
     await member.send(f"{member} banned from---{ctx.guild.name} reason:{reason}")
     await ctx.send(f'`bannned:{member} reason:{reason}`')
 
-@bot.command(name='unban')
+@AK.command(name='unban')
 async def unban(ctx, id: int):
-    user = await bot.fetch_user(id)
+    user = await AK.fetch_user(id)
     await ctx.message.delete()
     await ctx.guild.unban(user)
     await user.send(f"{user} umban from---`{ctx.guild.bame}`")
     await ctx.send(f"`unbanned:{user}`")
 
-@bot.command()
+@AK.command()
 async def role(ctx, member : discord.Member, role : discord.Role):
     if member is None:
         member = ctx.author
@@ -148,25 +156,25 @@ async def role(ctx, member : discord.Member, role : discord.Role):
     await member.add_roles(role)
     await ctx.send(f"`{member} addedrole:{role}`")
 
-@bot.command(description="Unmutes a specified user.")
+@AK.command(description="Unmutes a specified user.")
 @commands.has_permissions(manage_messages=True)
 async def m(ctx, member: discord.Member):
    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
 
    await member.add_roles(mutedRole)
    await ctx.send(f"`{member} Muted`")
-   await member.send(f"MUTED from---`{ctx.guild.name}`")
+#   await member.send(f"MUTED from---`{ctx.guild.name}`")
 
-@bot.command(description="Unmutes a specified user.")
+@AK.command(description="Unmutes a specified user.")
 @commands.has_permissions(manage_messages=True)
 async def um(ctx, member: discord.Member):
    mutedRole = discord.utils.get(ctx.guild.roles, name="Muted")
 
    await member.remove_roles(mutedRole)
    await ctx.send(f"`{member} UMMUTED`")
-   await member.send(f"UMMUTED from---`{ctx.guild.name}`")
+#   await member.send(f"UMMUTED from---`{ctx.guild.name}`")
 
-@bot.command(description="Unmutes a specified user.")
+@AK.command(description="Unmutes a specified user.")
 @commands.has_permissions(manage_messages=True)
 async def rr(ctx, member: discord.Member, role : discord.Role):
    mutedRole = discord.utils.get(ctx.guild.roles, name=f"{role}")
@@ -175,14 +183,19 @@ async def rr(ctx, member: discord.Member, role : discord.Role):
    await ctx.message.delete()
    await ctx.send(f"`{member} rr {role}`")
 
-@bot.command()
+@AK.command()
 async def clear(ctx, target:int):
     channel = ctx.message.channel
     deleted = await channel.purge(limit=target+1)
 
+@AK.command()
+async def ping(ctx,user :discord.Member = None):
+    await ctx.message.delete()
+    await ctx.send(f"<@{user.id}>")
+    await ctx.send(f"<@{user.id}>")
 
 #testcommanmd
-@bot.command()
+@AK.command()
 async def c(ctx, name, user :discord.Member = None):
     guild = ctx.guild
     try:
@@ -203,47 +216,75 @@ async def c(ctx, name, user :discord.Member = None):
         except:
             await message.edit("something error")
 
-@bot.command()
+@AK.command()
 async def ow(ctx):
-    guild_owner = bot.get_user(int(ctx.guild.owner.id))
+    guild_owner = AK.get_user(int(ctx.guild.owner.id))
     await ctx.send(f'The owner of this server is: {guild_owner}')
 
+@AK.command()
+async def ot(ctx, word, target:int):
+    await ctx.message.delete()
+    while True:
+        await ctx.send(f"{word}")
+        time.sleep(target)
 
-
-@bot.command()
+@AK.command()
 async def fe(ctx, *, word):
     await ctx.message.delete()
-    await ctx.send(f"||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||_ _ _ _ _ _ _ https://test.rauf.workers.dev/?&author={word}&color=00FF93")
+    await ctx.send(f"​_ _||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||||​||_ _ _ _ _ _ _ https://test.rauf.workers.dev/?&author={word}!&color=18F900")
 
-@bot.command()
+@AK.command()
 async def gs(ctx, *, word):
     await ctx.message.delete()
     await ctx.send(f"https://www.google.com/search?q={word}")
 
-@bot.command()
+@AK.command()
 async def ys(ctx, *, word):
     await ctx.message.delete()
     await ctx.send(f"https://www.youtube.com/results?search_query={word}")
 
-@bot.command()
-async def spam(ctx, *, word):
-    while True:
-        await ctx.send(word)
 
-@bot.command()
-async def bin(ctx, *, member: discord.Member = None):
-    if member is None:
-        member = ctx.author
+
+@AK.command()
+async def bin(ctx, *, user: discord.Member = None):
     await ctx.message.delete()
-    await ctx.send(f'https://discordapp.com/oauth2/authorize?client_id={member.id}&permissions=8&scope=bot')
-    await ctx.send(f"<@{member.id}>")
+    await ctx.send(f'https://discordapp.com/oauth2/authorize?client_id={user.id}&permissions=8&scope=bot')
+    await ctx.send(f"{user.mention}")
 
 #event＆run
-@bot.event
+@AK.event
 async def on_ready():
-    await bot.change_presence(activity=discord.Streaming(name=f"{bot.user}| {len(bot.guilds)} joinserver", url="https://discord.gg"))
+    while True:
+        await AK.change_presence(activity=discord.Streaming(name=f"{AK.user}| {len(AK.guilds)} joinserver", url="https://www.twitch.tv/kato_junichi0817"))
+        await asyncio.sleep(2)
+        await AK.change_presence(activity=discord.Streaming(name=f"にとろくれ", url="https://www.twitch.tv/kato_junichi0817"))
+
     print("""
-                                                                                                            __________
+
+  bin   
+  bk    
+  c     
+  cl    
+  clear 
+  day   
+  eu    
+  fe    
+  gb    
+  gs    
+  kami  
+  minfo 
+  nani  
+  ot    
+  ow    
+  ping  
+  role  
+  sinfo 
+  spam  
+  time  
+  ub    
+  ui    
+  unban 
+  ys                                                                                                     __________
    ______________     ________________     ____            __________________     _________               /   _______  \         __________________                                                                                                                                                                                                                                                                                              
   |              |   |                |   |    |          |                  |   |    __   \             /   /       \  \       |                  |                                                                                                                                                                                                                                                                                    
   |     _________|   |   _____________|   |    |          |     _____________|   |   |  |   |           /   /         \  \      |__________________|                                                                                                                                                                                                                                                                                                       
@@ -258,11 +299,13 @@ async def on_ready():
  |               |   |                |   |            |  |    |                 |   |___|      /        \   \_______/  /              |    |                                                                                                       
  |_______________|   |________________|   |____________|  |____|                 |_____________/          \ __________ /               |____| 
 
-day-self-bot owner AK#1000
-support https://discord.gg/4SE7U5TecJ
+
+
+dayself_bot prefix=^^  madeby A__K#1000
 
 """)
-    print(f"supportserver{server} made by {bot.user}")
-    print(f"accountdata {bot.user} | {bot.user.id}")
-    
-bot.run(token, bot=False)
+
+    print(f"accountdata {AK.user} | {AK.user.id}\njoin server {len(AK.guilds)}")
+
+
+AK.run(token, bot=False) 
